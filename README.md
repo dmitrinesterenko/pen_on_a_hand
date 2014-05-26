8a4b003fd50201f706ced9a554e5eba97ca8aafd
Dmitri Zabatay <dmitri@sentimentalistmag.com>
2014-05-25 23:48:28 -0400

    Add uikit. Parse for email from text

--- README.md
+++ README.md
@@ -10,5 +10,5 @@ Resources
 -----------
 * [Camping Guide](http://camping.io)
 * Adding middleware and publishing the app, please notice the suggestion for using run Appname in Camping 2.0. [Heroku Guid](https://devcenter.heroku.com/articles/rack#frameworks)
-
+* How to validate an email with [regex](http://ex-parrot.com/~pdw/Mail-RFC822-Address.html)
 
--- contacts.rb
+++ contacts.rb
@@ -8,22 +8,42 @@ module Contacts::Controllers
 		def get
 			#Time.now.to_s
 			@time = Time.now.to_s
-			@contacts = Contact.all
+			@contacts = Contact.all(:order => 'updated_at DESC')
 			       
 			render :create_new
 		end
 
 		def post
-			@flash = "New user created"
+			#@flash = "Note added"
 			contact_list = Contacts::Models::Contact.new
     			#contact_list.user_id ='0' 
-			contact_list.email = 'email@mailinator.com'
+			#contact_list.email = 'email@mailinator.com'
+
+			contact_list.note = input.notes
+			contact_list.email = capture_email contact_list.note
 			contact_list.save
-			@contacts = Contact.all
+			@contacts = Contact.all(:order => 'updated_at DESC')
 			render :create_new
 		end
-	end
 
+		def capture_email(input)
+    			email_match = /.*(\s[\w._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}).*/.match(input)
+			if email_match.length > 0
+				email_match[1]
+			else
+				''
+			end
+    		end
+	end
+	# This is computed every time, oh boy, it's much better to do these as
+	# STATICS so that the value is computed once when the app is launched
+	class Style < R '/uikit-2.6.0/(.*)'
+	      	   def get(asset_path)
+			asset = File.read(File.dirname(__FILE__)+'/uikit-2.6.0/'+asset_path).gsub(/.*__END__/m, '') 
+		        @headers['Content-Type'] = 'text/css; charset=utf-8'
+			asset
+		   end
+	end
 	#class Create < R '/create'
 #		def post
 #			p "New user created"
@@ -37,25 +57,27 @@ module Contacts::Views
 	def layout
 		html do
 			head do
-				title { "Contact Manager" }
+				title { "Pen a Note" }
+				link :rel => 'stylesheet', :type => 'text/css', :href => "/uikit-2.6.0/css/uikit.css"
+				script :src => "/uikit-2.6.0/js/uikit.js" 
 			end
 			body { self << yield }
 		end
 	end
 
 	def create_new
-		p "enter a name"
-		if @flash 
-			p @flash
-		end
-		@contacts.each do |contact|
-			p contact.id
-			p contact.email
-		end
 		form :method=>'post' do
-			input type: 'input', name: 'name', value: '' 
+			input type: 'textarea', name: 'notes', value: '' 
 			input type: 'submit', name: 'submit', value: 'Add'
 		end	
+		if @flash 
+			#p @flash
+		end
+		@contacts.each do |contact|
+			div contact.note.to_s 
+			div contact.email.to_s
+		end
+	
 	end
 end
 
@@ -93,6 +115,19 @@ module Contacts::Models
 			drop_table ContactList.table_name
 			drop_table Contact.table_name
 		end
+	end
+
+	class AddNoteField < V 1.1
+		def self.up
+			#change_table Contact.table_name do |t|
+			add_column Contact.table_name, :note, :string, null: true
+				
+		end
+
+		def self.down
+			remove_column Contact.table_name, :note	
+		end
+
 	end
 end
 
